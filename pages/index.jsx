78d5c670202db28777473f682009ea43c9ac4c71
Chris <chrisjobando@gmail.com>
2019-10-06 20:19:43 -0400

    Removed ItemsModal from homepage, and replaced with a link to the applications page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,42 +1,21 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Container } from 'reactstrap';
+import { Container, Button } from 'reactstrap';
 import AppNavbar from '../frontend/components/AppNavbar';
-import ItemsList from '../frontend/components/ItemsList';
-import ItemModal from '../frontend/components/ItemModal';
-import { getItems } from '../frontend/actions/items';
 
 class IndexPage extends React.Component {
-  static async getInitialProps({ req }) {
-    if (req) {
-      // TODO server-side database calls
-      const items = await getItems();
-
-      return {
-        items,
-      };
-    }
-  }
-
   render() {
-    const { items } = this.props;
-
-    console.log(items);
-
     return (
       <div className="App">
         <AppNavbar />
         <Container>
-          <ItemModal />
-          <ItemsList />
+          <a href="/application">
+            <Button>Apply Here</Button>
+          </a>
         </Container>
       </div>
     );
   }
 }
-
-IndexPage.propTypes = {
-  items: PropTypes.array.isRequired,
-};
 
 export default IndexPage;

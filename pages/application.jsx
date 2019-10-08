ab97a3c818a9e757e7e1edcc27c4b53bd557527e
Chris <chrisjobando@gmail.com>
2019-10-08 18:37:47 -0400

    Minor fixes

--- pages/application.jsx
+++ pages/application.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Container } from 'reactstrap';
 import AppNavbar from '../frontend/components/AppNavbar';
 import WelcomeJumbo from '../frontend/components/WelcomeJumbo'
--- pages/submitted.jsx
+++ pages/submitted.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Container } from 'reactstrap';
+import { Container, Progress } from 'reactstrap';
 import AppNavbar from '../frontend/components/AppNavbar';
 
 class IndexPage extends React.Component {
@@ -10,16 +9,12 @@ class IndexPage extends React.Component {
         <AppNavbar />
         <Container style={{ textAlign:"center" }}>
           <h1>Thank you so much for your interest!</h1>
-          <h2>Progress bar (lol)</h2>
+          <Progress animated color="success" value="25" className="progress-bar" />
           <p> Your application has been submitted to the BoG team successfully! You will get an email notification after we finish reviewing your application. If we decide to move on with your project, the next step will be an interview to better understand your project and if it’s a good fit for Bits of Good.</p>
         </Container>
       </div>
     );
   }
 }
-
-IndexPage.propTypes = {
-  items: PropTypes.array.isRequired,
-};
 
 export default IndexPage;

4c35b82c8950b36694ea7493d07a23f9ce091391
Chris <chrisjobando@gmail.com>
2019-10-15 21:10:26 -0400

    Changed format of how individual application items are displayed

--- frontend/components/ApplicationsList.jsx
+++ frontend/components/ApplicationsList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  Collapse,
   Container,
   ListGroup,
   ListGroupItem,
@@ -8,16 +9,29 @@ import {
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { getApplications, deleteApplication } from '../redux/actions/applicationActions';
 
 class ApplicationsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
   componentDidMount() {
     this.props.getApplications();
-  };
+  }
 
   onDeleteClick = (id) => {
     this.props.deleteApplication(id);
-  };
+  }
+
+  toggleCollapse = () => {
+    this.setState((state) => ({ open: !state.open }));
+  }
 
   render() {
     const { applications } = this.props.application;
@@ -48,53 +62,64 @@ class ApplicationsList extends Component {
             }) => (
               <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
-                  <br />
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={this.onDeleteClick.bind(this, _id)}
-                  >
-                  Delete
+                  <div style={{ display: 'flex', margin: '30px 0' }}>
+                    <Button
+                      className="remove-btn"
+                      color="danger"
+                      size="sm"
+                      style= {{ marginRight: '20px' }}
+                      onClick={this.onDeleteClick.bind(this, _id)}
+                    >
+                      <h5>Delete</h5>
+                    </Button>
+                    <h2 style={{ fontWeight: '600' }}>{name}</h2>
+                  </div>
+                  <Button color="#F0F4F7" onClick={() => this.toggleCollapse()} style={{ marginBottom: '1rem' }} block>
+                    <div align="left">
+                      <h4>
+                        {this.state.open ? <FontAwesomeIcon icon={faChevronDown} size="sm" />
+                          : <FontAwesomeIcon icon={faChevronRight} size="sm" />}
+                        {' '}
+                      View Application
+                      </h4>
+                    </div>
                   </Button>
-                  <br />
-                  <br />
-                  <p style={{ fontWeight: '600' }}>Name: </p>
-                  <p>{name}</p>
-                  <p style={{ fontWeight: '600' }}>Address: </p>
-                  <p>{address}</p>
-                  <p style={{ fontWeight: '600' }}>Website: </p>
-                  <p><a href={website} target="_blank" rel="noopener noreferrer" >{website}</a></p>
-                  <p style={{ fontWeight: '600' }}>Work Phone: </p>
-                  <p>{workPhone}</p>
-                  <p style={{ fontWeight: '600' }}>Contact Name: </p>
-                  <p>{contactName}</p>
-                  <p style={{ fontWeight: '600' }}>Mobile Phone: </p>
-                  <p>{mobilePhone}</p>
-                  <p style={{ fontWeight: '600' }}>Email: </p>
-                  <p>{email}</p>
-                  <p style={{ fontWeight: '600' }}>Mission: </p>
-                  <p>{mission}</p>
-                  <p style={{ fontWeight: '600' }}>Needs Web? </p>
-                  <p>{needsWeb}</p>
-                  <p style={{ fontWeight: '600' }}>Needs Mobile? </p>
-                  <p>{needsMobile}</p>
-                  <p style={{ fontWeight: '600' }}>Needs Other? </p>
-                  <p>{needsOther}</p>
-                  <p style={{ fontWeight: '600' }}>If so what: </p>
-                  <p>{needsOtherExpand}</p>
-                  <p style={{ fontWeight: '600' }}>Stage of Development: </p>
-                  <p>{stageRadio}</p>
-                  <p style={{ fontWeight: '600' }}>If other, what: </p>
-                  <p>{stageOtherExpand}</p>
-                  <p style={{ fontWeight: '600' }}>Availability: </p>
-                  <p>{availRadio}</p>
-                  <p style={{ fontWeight: '600' }}>Field Test? </p>
-                  <p>{fieldRadio}</p>
-                  <p style={{ fontWeight: '600' }}>Other Product Needs: </p>
-                  <p>{productExtra}</p>
-                  <p style={{ fontWeight: '600' }}>Feedback: </p>
-                  <p>{feedback}</p>
+                  <Collapse isOpen={this.state.open}>
+                    <p style={{ fontWeight: '600' }}>Address: </p>
+                    <p>{address}</p>
+                    <p style={{ fontWeight: '600' }}>Website: </p>
+                    <p><a href={website} target="_blank" rel="noopener noreferrer">{website}</a></p>
+                    <p style={{ fontWeight: '600' }}>Work Phone: </p>
+                    <p>{workPhone}</p>
+                    <p style={{ fontWeight: '600' }}>Contact Name: </p>
+                    <p>{contactName}</p>
+                    <p style={{ fontWeight: '600' }}>Mobile Phone: </p>
+                    <p>{mobilePhone}</p>
+                    <p style={{ fontWeight: '600' }}>Email: </p>
+                    <p>{email}</p>
+                    <p style={{ fontWeight: '600' }}>Mission: </p>
+                    <p>{mission}</p>
+                    <p style={{ fontWeight: '600' }}>Needs Web? </p>
+                    <p>{needsWeb}</p>
+                    <p style={{ fontWeight: '600' }}>Needs Mobile? </p>
+                    <p>{needsMobile}</p>
+                    <p style={{ fontWeight: '600' }}>Needs Other? </p>
+                    <p>{needsOther}</p>
+                    <p style={{ fontWeight: '600' }}>If so what: </p>
+                    <p>{needsOtherExpand}</p>
+                    <p style={{ fontWeight: '600' }}>Stage of Development: </p>
+                    <p>{stageRadio}</p>
+                    <p style={{ fontWeight: '600' }}>If other, what: </p>
+                    <p>{stageOtherExpand}</p>
+                    <p style={{ fontWeight: '600' }}>Availability: </p>
+                    <p>{availRadio}</p>
+                    <p style={{ fontWeight: '600' }}>Field Test? </p>
+                    <p>{fieldRadio}</p>
+                    <p style={{ fontWeight: '600' }}>Other Product Needs: </p>
+                    <p>{productExtra}</p>
+                    <p style={{ fontWeight: '600' }}>Feedback: </p>
+                    <p>{feedback}</p>
+                  </Collapse>
                 </ListGroupItem>
               </CSSTransition>
             ))}

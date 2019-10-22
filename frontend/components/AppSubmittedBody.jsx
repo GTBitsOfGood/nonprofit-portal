1aa88b68fb41ddc7aeefd97e79d84577afed3ddf
Max Karpawich <max.karpawich@gmail.com>
2019-10-22 18:17:00 -0400

    landing page descriptions

--- frontend/components/AppSubmittedBody.jsx
+++ frontend/components/AppSubmittedBody.jsx
@@ -1,20 +1,12 @@
-import { Center } from 'react';
+import LandingBodyMessage from './LandingBodyMessage';
 
-const AppSubmittedBody = () => {
-  return (
-    <p style={{
-      width: '750px',
-      textAlign: 'center',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    }}
-    >
+const AppSubmittedBody = () => (
+  <LandingBodyMessage width={750}>
     Your application has been submitted to the BoG team successfully!
     You will get an email notification after we finish reviewing your application.
-    If we decide to move on with your project, the next step will be an interview
-    to better understand your project and if it’s a good fit for Bits of Good.
-    </p>
-  );
-};
+    If we decide to move on with your project, the next step will be an interview to
+    better understand your project and if it’s a good fit for Bits of Good.
+  </LandingBodyMessage>
+);
 
 export default AppSubmittedBody;
---
+++ frontend/components/LandingBodyMessage.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const LandingBodyMessage = (props) => {
+  const { children, width } = props;
+
+  return (
+    <p style={{
+      width: `${width}px`,
+      textAlign: 'center',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    }}
+    >
+      {children}
+    </p>
+  );
+};
+
+LandingBodyMessage.propTypes = {
+  children: PropTypes.array.isRequired,
+  width: PropTypes.number.isRequired,
+};
+
+
+export default LandingBodyMessage;

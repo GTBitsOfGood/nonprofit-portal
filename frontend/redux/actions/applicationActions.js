0873aed13adc50a75f6a555c8dad0db560913ea0
Josh <jmcfarlin6@gatech.edu>
2019-10-24 17:13:14 -0400

    Fixed Redux action dispatch

--- frontend/redux/actions/applicationActions.js
+++ frontend/redux/actions/applicationActions.js
@@ -8,30 +8,25 @@ export const setApplicationsLoading = () => ({
 });
 
 export const getApplications = () => (dispatch) => {
-  dispatch(setApplicationsLoading());
-
-  applicationActions
-    .getApplications()
-    .then((res) => dispatch({
-      type: GET_APPLICATIONS,
-      payload: res,
-    }));
+  dispatch(setApplicationsLoading())
+    .then(() => applicationActions
+      .getApplications()
+      .then((res) => dispatch({
+        type: GET_APPLICATIONS,
+        payload: res,
+      })));
 };
 
-export const addApplication = (application) => (dispatch) => {
-  return applicationActions
-    .addApplication(application)
-    .then((res) => dispatch({
-      type: ADD_APPLICATION,
-      payload: res,
-    }));
-};
+export const addApplication = (application) => (dispatch) => applicationActions
+  .addApplication(application)
+  .then((res) => dispatch({
+    type: ADD_APPLICATION,
+    payload: res,
+  }));
 
-export const deleteApplication = (id) => (dispatch) => {
-  applicationActions
-    .deleteApplication(id)
-    .then(() => dispatch({
-      type: DELETE_APPLICATION,
-      payload: id,
-    }));
-};
+export const deleteApplication = (id) => (dispatch) => applicationActions
+  .deleteApplication(id)
+  .then(() => dispatch({
+    type: DELETE_APPLICATION,
+    payload: id,
+  }));

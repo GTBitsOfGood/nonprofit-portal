f78323ab7194c5ceee640d360252848b167ce89d
Max Karpawich <max.karpawich@gmail.com>
2019-10-25 17:55:59 -0400

    email backend support

---
+++ server/util/email.js
@@ -0,0 +1,36 @@
+const Email = require('email-templates');
+const { join } = require('path');
+
+const fromAddress = '"GT Bits of Good" <TODO@bitsofgood.org>';
+
+const transportConfig = {
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+};
+
+const sendEmail = (options) => {
+  const email = new Email({
+    message: {
+      from: fromAddress,
+    },
+    transport: transportConfig,
+    send: true,
+  });
+
+  return email.send(
+    {
+      template: join(__dirname, '..', '..', 'emails', options.template),
+      message: {
+        to: options.to,
+      },
+      locals: options.locals,
+    },
+  );
+};
+
+module.exports = { sendEmail };

4079090e656c6e6a51be832d95066351e21ae2b3
Josh-McFarlin <jmcfarlin6@gatech.edu>
2019-11-04 21:54:48 -0500

    Setup availability server functions

--- server/mongodb/actions/availability.js
+++ server/mongodb/actions/availability.js
@@ -1,61 +1,51 @@
-import config from '../../../config';
-
 const mongoose = require('mongoose');
 const mongoDB = require('../index');
-const Application = require('../models/Application');
-
-const { generateURLString } = require('./util');
-const { sendEmail } = require('../../util/email');
+const Availability = require('../models/Availability');
 
 async function getAvailabilities() {
   await mongoDB();
 
-  let applications = [];
-  await Application
-    .find()
-    .sort({ date: -1 })
+  let availabilities = [];
+  await Availability
+    .find({
+      startDate: {
+        $gte: new Date(),
+      },
+    })
+    .sort({
+      startDate: -1,
+    })
     .then((res) => {
-      applications = res;
+      availabilities = res;
       mongoose.connection.close();
     })
     .catch(() => {
       mongoose.connection.close();
     });
 
-  return applications;
+  return availabilities;
 }
 
-async function addAvailability(application) {
+async function addAvailability(availability) {
   await mongoDB();
 
   let result = {};
 
   try {
-    const pageURLString = await generateURLString();
-    const newApplication = new Application(application);
-    newApplication.urlString = pageURLString;
-    result = await newApplication.save();
-    sendEmail({
-      to: newApplication.email,
-      template: 'status',
-      locals: {
-        status: 0,
-        name: newApplication.name,
-        baseUrl: config.baseUrl,
-        urlString: newApplication.urlString,
-      },
-    });
+    const newAvailability = new Availability(availability);
+    result = await newAvailability.save();
   } finally {
     mongoose.connection.close();
   }
+
   return result;
 }
 
 async function deleteAvailability(id) {
   await mongoDB();
 
-  await Application.findById(id)
-    .then((application) => application.remove())
+  await Availability.findById(id)
+    .then((availability) => availability.remove())
     .then(() => {
       mongoose.connection.close();
     })
@@ -64,19 +54,13 @@ async function deleteAvailability(id) {
     });
 }
 
-async function updateAvailability(id, state) {
+async function updateAvailability(id, isBooked) {
   await mongoDB();
   let result = {};
 
   try {
-    const curObject = await Application.findOne({ _id: id }, { status: 1 });
-    if (curObject.status < 3) {
-      result = await Application.findOneAndUpdate({ _id: id }, { status: state, decision: null },
-        { upsert: false, new: true, useFindAndModify: false });
-    } else {
-      result = await Application.findOneAndUpdate({ _id: id }, { status: state },
-        { upsert: false, new: true, useFindAndModify: false });
-    }
+    result = await Availability.findOneAndUpdate({ _id: id }, { isBooked },
+      { upsert: false, new: true, useFindAndModify: false });
   } finally {
     mongoose.connection.close();
   }

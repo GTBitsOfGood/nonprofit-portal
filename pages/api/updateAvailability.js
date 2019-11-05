549559fc22704eb63fa568f01a70576b50495790
Josh-McFarlin <jmcfarlin6@gatech.edu>
2019-11-04 22:44:13 -0500

    Changed REST call type in documentation

--- pages/api/updateAvailability.js
+++ pages/api/updateAvailability.js
@@ -1,6 +1,6 @@
 import { updateAvailability } from '../../server/mongodb/actions/availabilities';
 
-// @route   GET api/updateAvailability
+// @route   POST api/updateAvailability
 // @desc    Update the status of an availability
 // @access  Public
 export default async function (req, res) {

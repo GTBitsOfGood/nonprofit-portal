7cb5902445b3d481344fccd4ebc772f581d857f4
Josh-McFarlin <jmcfarlin6@gatech.edu>
2019-11-04 23:10:53 -0500

    Added availabilities to Redux

---
+++ frontend/redux/reducers/availabilityReducer.js
@@ -0,0 +1,50 @@
+import {
+  GET_AVAILABILITIES, ADD_AVAILABILITY, DELETE_AVAILABILITY,
+  UPDATE_AVAILABILITY, AVAILABILITIES_LOADING,
+} from '../actions/types';
+
+const initialState = {
+  availabilities: [],
+  loading: false,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_AVAILABILITIES:
+      return {
+        ...state,
+        availabilities: action.payload,
+        loading: false,
+      };
+    case DELETE_AVAILABILITY:
+      return {
+        ...state,
+        availabilities: state.applications.filter(
+          (availability) => availability._id !== action.payload,
+        ),
+      };
+    case ADD_AVAILABILITY:
+      return {
+        ...state,
+        availabilities: [action.payload, ...state.availabilities],
+      };
+    case AVAILABILITIES_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case UPDATE_AVAILABILITY:
+      return {
+        ...state,
+        availabilities: state.availabilities.map((availability) => {
+          if (availability._id === action.payload._id) {
+            return action.payload;
+          }
+
+          return availability;
+        }),
+      };
+    default:
+      return state;
+  }
+}

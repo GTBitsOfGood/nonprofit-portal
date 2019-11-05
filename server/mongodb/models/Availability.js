d147276466b11eb21d747aee1c7c24dee1b9a3ec
Josh-McFarlin <jmcfarlin6@gatech.edu>
2019-11-04 21:38:01 -0500

    Added availability model

---
+++ server/mongodb/models/Availability.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+// Create Schema
+const AvailabilitySchema = new Schema({
+  team: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: false,
+  },
+  isBooked: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+let Availability;
+
+if (mongoose.models.Availability) {
+  Availability = mongoose.model('Availability');
+} else {
+  Availability = mongoose.model('Availability', AvailabilitySchema);
+}
+
+module.exports = Availability;

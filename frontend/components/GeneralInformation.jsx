8a1170c7c2e9f878b7c12f3e58c9e0e6938d664a
Vince Li <vince.li.ct@gmail.com>
2019-11-07 15:50:37 -0500

    inline

--- frontend/components/GeneralInformation.jsx
+++ frontend/components/GeneralInformation.jsx
@@ -72,75 +72,86 @@ class GeneralInformation extends Component {
             />
           </FormGroup>
           <FormGroup>
-            <Label for="application">City</Label>
-            <Input
-              type="text"
-              name="city"
-              id="application"
-              placeholder="(Optional)"
-              onChange={onChange}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="application">State</Label>
-            <CustomInput
-              type="select"
-              name="state"
-              id="application"
-              placeholder="(Optional)"
-              onChange={onChange}
-            >
-              <option>AL</option>
-              <option>AK</option>
-              <option>AZ</option>
-              <option>AR</option>
-              <option>CA</option>
-              <option>CO</option>
-              <option>CT</option>
-              <option>DE</option>
-              <option>FL</option>
-              <option>GA</option>
-              <option>HI</option>
-              <option>ID</option>
-              <option>IL</option>
-              <option>IN</option>
-              <option>IA</option>
-              <option>KS</option>
-              <option>KY</option>
-              <option>LA</option>
-              <option>ME</option>
-              <option>MD</option>
-              <option>MA</option>
-              <option>MI</option>
-              <option>MN</option>
-              <option>MS</option>
-              <option>MO</option>
-              <option>MT</option>
-              <option>NE</option>
-              <option>NV</option>
-              <option>NH</option>
-              <option>NJ</option>
-              <option>NM</option>
-              <option>NY</option>
-              <option>NC</option>
-              <option>ND</option>
-              <option>OH</option>
-              <option>OK</option>
-              <option>OR</option>
-              <option>PA</option>
-              <option>RI</option>
-              <option>SC</option>
-              <option>SD</option>
-              <option>TN</option>
-              <option>TX</option>
-              <option>UT</option>
-              <option>VT</option>
-              <option>VA</option>
-              <option>WA</option>
-              <option>WV</option>
-              <option>WI</option>
-              <option>WY</option>
-            </CustomInput>
+            <Label for="application">City & State</Label>
+            <div>
+              <CustomInput
+                type="text"
+                name="city"
+                id="application"
+                placeholder=""
+                onChange={onChange}
+                label="city"
+                style={{
+                  borderRadius: '.25rem',
+                  border: '1px solid #ced4da',
+                  padding: '6px',
+                  marginRight: '1%',
+                  paddingTop: '.375rem',
+                }}
+                inline
+              />
+              <CustomInput
+                type="select"
+                name="state"
+                id="application"
+                placeholder="(Optional)"
+                onChange={onChange}
+                style={{ width: '75px', marginLeft: '1%' }}
+                inline
+              >
+                <option>AL</option>
+                <option>AK</option>
+                <option>AZ</option>
+                <option>AR</option>
+                <option>CA</option>
+                <option>CO</option>
+                <option>CT</option>
+                <option>DE</option>
+                <option>FL</option>
+                <option>GA</option>
+                <option>HI</option>
+                <option>ID</option>
+                <option>IL</option>
+                <option>IN</option>
+                <option>IA</option>
+                <option>KS</option>
+                <option>KY</option>
+                <option>LA</option>
+                <option>ME</option>
+                <option>MD</option>
+                <option>MA</option>
+                <option>MI</option>
+                <option>MN</option>
+                <option>MS</option>
+                <option>MO</option>
+                <option>MT</option>
+                <option>NE</option>
+                <option>NV</option>
+                <option>NH</option>
+                <option>NJ</option>
+                <option>NM</option>
+                <option>NY</option>
+                <option>NC</option>
+                <option>ND</option>
+                <option>OH</option>
+                <option>OK</option>
+                <option>OR</option>
+                <option>PA</option>
+                <option>RI</option>
+                <option>SC</option>
+                <option>SD</option>
+                <option>TN</option>
+                <option>TX</option>
+                <option>UT</option>
+                <option>VT</option>
+                <option>VA</option>
+                <option>WA</option>
+                <option>WV</option>
+                <option>WI</option>
+                <option>WY</option>
+                <option>N/A</option>
+              </CustomInput>
+            </div>
           </FormGroup>
           <FormGroup>
             <Label for="application">Website</Label>

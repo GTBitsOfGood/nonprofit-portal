761afa676b7b8902cea795f028ae99d7863d7e07
Max Karpawich <max.karpawich@gmail.com>
2019-11-17 18:17:46 -0500

    update AdminCalendar import

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppNavbar from '../frontend/components/AppNavbar';
-import AdminCalendar from '../frontend/components/calendar/AdminCalendar';
+import AdminCalendar from '../frontend/components/Calendar/AdminCalendar';
 
 function IndexPage() {
   return (

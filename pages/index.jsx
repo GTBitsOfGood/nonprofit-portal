0e9754dd4cf7629b1abe9adea1f1097eeae574c2
Josh-McFarlin <jmcfarlin6@gatech.edu>
2019-11-24 22:58:38 -0500

    Made index redirect to apply page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Router from 'next/router';
+import config from '../config';
 
-function IndexPage() {
-  return (
-    <div />
-  );
+class IndexPage extends React.PureComponent {
+  static async getInitialProps(ctx) {
+    if (ctx.res) {
+      ctx.res.writeHead(302, {
+        Location: config.pages.application,
+      });
+      ctx.res.end();
+    } else {
+      await Router.push(config.pages.application);
+    }
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
 }
 
 export default IndexPage;

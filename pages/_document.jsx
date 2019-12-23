5990fbfc0a7fda45e70d692f3a44f671a8caeefd
Josh-McFarlin <jmcfarlin6@gatech.edu>
2019-12-23 02:50:39 -0500

    Added title and favicon

---
+++ pages/_document.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>BoG NPP</title>
+          <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

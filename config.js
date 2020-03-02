2e3c12ef4654e9763d89cb91adb39f65a39a97a4
Holben888 <holben888@gmail.com>
2020-03-01 22:06:45 -0500

    add env var for production url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 export default {
-  baseUrl: prod ? 'https://bog-npp.now.sh' : 'http://localhost:3000',
+  baseUrl: prod ? process.env.PROD_BASE_URL : 'http://localhost:3000',
   dbUrl: prod ? process.env.MONGO_DB : process.env.MONGO_DEV_DB || 'mongodb://localhost:27017',
   dbName: 'Applications',
   apis: {

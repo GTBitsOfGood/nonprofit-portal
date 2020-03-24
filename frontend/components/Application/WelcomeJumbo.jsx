46aebb210db966dabc96e476bb66300239a441a5
Max Karpawich <max.karpawich@gmail.com>
2020-03-24 01:18:01 -0400

    add BoG logo

--- frontend/components/Application/WelcomeJumbo.jsx
+++ frontend/components/Application/WelcomeJumbo.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const WelcomeJumbo = () => (
   <div>
     <div style={{ background: 'transparent' }} className="Jumbotron">
-      <h4 align="center">Online Application</h4>
+      <img src="/static/bog_logo.svg" alt="Bits of Good" style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom: '5px', width: '230px'}} />
+      <h4 align="center">Nonprofit Application</h4>
       <p className="lead" align="center">
         As a partner, Bits of Good will help you build software that turns your need into
         real product. Please fill out the following form so that we can best understand
